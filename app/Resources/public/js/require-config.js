cfbcfe2c1911875511f1013cf39e5ac07f9e7410
Michal Szymczak <pro@maikeru.pl>
2013-05-27 23:37:49 +0200

    Added html runner, slightly unstable.

--- app/Resources/public/js/require-config.js
+++ app/Resources/public/js/require-config.js
@@ -3,10 +3,12 @@ var require = {
     paths: {
         'AcmeSomeModuleFooBundle': 'Acme/SomeModule/FooBundle/Resources/public/js',
         'jquery': '../app/Resources/public/js/jquery',
+        'jasmine-jquery': '../app/Resources/public/js/jasmine-jquery',
         'lodash': '../app/Resources/public/js/lodash'
     },
     shim: {
         'lodash': {},
-        'jquery': {}
+        'jquery': {},
+        'jasmine-jquery': {}
     }
 };
